4491665eca6885c9ffe6f1fa07673d26ce616fcf
piy4l <1705034@ugrad.cse.buet.ac.bd>
2023-02-22 20:43:53 +0600

    another model loaded

--- Tutorials/04_sentence_recognition/configs.py
+++ Tutorials/04_sentence_recognition/configs.py
@@ -16,3 +16,17 @@ class ModelConfigs(BaseModelConfigs):
         self.learning_rate = 0.001
         self.train_epochs = 5
         self.train_workers = 20
+
+class ModelConfigs2(BaseModelConfigs):
+    def __init__(self):
+        super().__init__()
+        # self.model_path = stow.join('Models/04_sentence_recognition', datetime.strftime(datetime.now(), "%Y%m%d%H%M"))
+        self.model_path = stow.join('Models', 'yaml2')
+        self.vocab = ''
+        self.height = 96
+        self.width = 1408
+        self.max_text_length = 0
+        self.batch_size = 32
+        self.learning_rate = 0.001
+        self.train_epochs = 5
+        self.train_workers = 20
--- Tutorials/04_sentence_recognition/model.py
+++ Tutorials/04_sentence_recognition/model.py
@@ -2,6 +2,14 @@ from keras import layers
 from keras.models import Model
 
 from mltu.model_utils import residual_block
+from keras_preprocessing.sequence import pad_sequences
+
+from keras.layers import Dense, LSTM, Reshape, BatchNormalization, Input, Conv2D, MaxPool2D, Lambda, Bidirectional
+from keras.models import Model
+from keras.activations import relu, sigmoid, softmax
+import keras.backend as K
+from keras.utils import to_categorical
+from keras.callbacks import ModelCheckpoint
 
 def train_model(input_dim, output_dim, activation='leaky_relu', dropout=0.2):
     
@@ -33,6 +41,50 @@ def train_model(input_dim, output_dim, activation='leaky_relu', dropout=0.2):
     blstm = layers.Dropout(dropout)(blstm)
 
     output = layers.Dense(output_dim + 1, activation='softmax', name="output")(blstm)
+
+    model = Model(inputs=inputs, outputs=output)
+    return model
+
+
+def train_model_2(input_dim, output_dim, activation='leaky_relu', dropout=0.2, char_list= ''):
+    
+    inputs = layers.Input(shape=input_dim, name="input")
+
+    # normalize images here instead in preprocessing step
+    input = layers.Lambda(lambda x: x / 255)(inputs)
+    
+    # convolution layer with kernel size (3,3)
+    conv_1 = Conv2D(32, (3,3), activation = 'relu', padding='same')(input)
+    # poolig layer with kernel size (2,2)
+    pool_1 = MaxPool2D(pool_size=(2, 2), strides=2)(conv_1)
+    
+    conv_2 = Conv2D(32, (3,3), activation = 'relu', padding='same')(pool_1)
+    pool_2 = MaxPool2D(pool_size=(2, 2), strides=2)(conv_2)
+    
+    conv_3 = Conv2D(64, (3,3), activation = 'relu', padding='same')(pool_2)
+    
+    conv_4 = Conv2D(64, (3,3), activation = 'relu', padding='same')(conv_3)
+    # poolig layer with kernel size (2,1)
+    pool_4 = MaxPool2D(pool_size=(2, 1))(conv_4)
+    
+    conv_5 = Conv2D(128, (3,3), activation = 'relu', padding='same')(pool_4)
+    # Batch normalization layer
+    batch_norm_5 = BatchNormalization()(conv_5)
+    
+    conv_6 = Conv2D(128, (3,3), activation = 'relu', padding='same')(batch_norm_5)
+    batch_norm_6 = BatchNormalization()(conv_6)
+    pool_6 = MaxPool2D(pool_size=(2, 1))(batch_norm_6)
+    
+    conv_7 = Conv2D(256, (2,2), activation = 'relu')(pool_6)
+    
+    #squeezed = Lambda(lambda x: K.squeeze(x, 1))(conv_7)
+    squeezed = layers.Reshape((conv_7.shape[-3] * conv_7.shape[-2], conv_7.shape[-1]))(conv_7)
+    
+    # bidirectional LSTM layers with units=128
+    blstm_1 = Bidirectional(LSTM(128, return_sequences=True, dropout = 0.2))(squeezed)
+    blstm_2 = Bidirectional(LSTM(128, return_sequences=True, dropout = 0.2))(blstm_1)
+    
+    output = Dense(len(char_list)+1, activation = 'softmax')(blstm_2)
 
     model = Model(inputs=inputs, outputs=output)
     return model
